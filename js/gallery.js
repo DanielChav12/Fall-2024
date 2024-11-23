472a8aee76dbb6043caf8520d99d6a79645b47b1
DanielChav12 <Yellowjacketblue12@gmail.com>
2024-11-23 09:00:14 -0600

    added gallery

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // console.log( "ready!" );
+
+
+   $("#galleryIMED1316").nanogallery2({
+
+    thumbnailHeight:  "auto",
+    thumbnailWidth:   "450",
+    thumbnailBorderVertical: "0",
+    thumbnailBorderHorizontal: "0",
+    thumnailLabel: {
+        postition: "overImageOnBottom",
+        display: false
+    },
+    thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|LabelApper75",
+    thumbnailGutterWidth: 20,
+    thumbnailHeight: 20,
+    thumbnailAlignment: "center",
+    thumbnailOpenImage: true
+
+
+  });
+
+
+
+
+});
